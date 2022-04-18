30acb377a6a1fbad431c06482f48470f3057ddca
marta-gavricheva <103070605+marta-gavricheva@users.noreply.github.com>
2022-04-18 22:58:22 +0300

    Add files via upload

---
+++ PP_2_1_4_spring-types-of-wiring/src/main/java/koschei/models/Deth8.java
@@ -0,0 +1,11 @@
+package koschei.models;
+
+import koschei.KoscheiTheDeathless;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Deth8 {
+
+
+    }
+
---
+++ PP_2_1_4_spring-types-of-wiring/src/main/java/koschei/models/Ocean1.java
@@ -0,0 +1,21 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Ocean1 {
+
+  @Autowired
+    private Island2 island2;
+
+//    @Autowired
+//    public Ocean1(Island2 island2) {
+//        this.island2 = island2;
+//    }
+
+    @Override
+    public String toString() {
+        return "на океане остров " + island2.toString();
+    }
+}
